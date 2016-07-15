2fec26bfcb059b5a9e08bebda0a33003d2770cf0
Jennelle Crothers <jkcrothers@hotmail.com>
2016-07-14 20:06:41 -0700

    Updated StopVMs.ps1 in Azure Automation.

--- StopVMs.ps1
+++ StopVMs.ps1
@@ -3,4 +3,5 @@
  -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
  
  Stop-AzureRmVM -Name 'UbuntuPlayground' -ResourceGroupName 'OSSWorld' -Force
-
+ Stop-AzureRmVM -Name 'JenkMastervm' -ResourceGroupName 'Jenkins' -Force
+ Stop-AzureRmVM -Name 'JenkSlave0vm' -ResourceGroupName 'Jenkins' -Force
