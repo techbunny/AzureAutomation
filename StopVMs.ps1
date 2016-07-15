0cbe617fd76439f31c7a2df5c6f9ee832f067837
Jennelle Crothers <jkcrothers@hotmail.com>
2016-07-14 19:34:31 -0700

    Created StopVMs.ps1 in AzureAutomation.

---
+++ StopVMs.ps1
@@ -0,0 +1,5 @@
+ $Conn = Get-AutomationConnection -Name AzureRunAsConnection
+ Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+ -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+ 
+ Start-AzureRmVM -Name 'UbuntuPlayground' -ResourceGroupName 'OSSWorld'
