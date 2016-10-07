7ed695b4c85982678a6ca99993850a6fd30c691a
Jennelle Crothers <jkcrothers@hotmail.com>
2016-10-07 18:33:55 -0400

    Updated StopVMs.ps1 in Azure Automation.

--- StopVMs.ps1
+++ StopVMs.ps1
@@ -2,12 +2,17 @@
  Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
  -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
  
- Stop-AzureRmVM -Name 'UbuntuPlayground' -ResourceGroupName 'OSSWorld' -Force
- Stop-AzureRmVM -Name 'JenkMastervm' -ResourceGroupName 'Jenkins' -Force
- Stop-AzureRmVM -Name 'JenkSlave0vm' -ResourceGroupName 'Jenkins' -Force
- Stop-AzureRmVM -Name 'LinuxA' -ResourceGroupName 'PeeringLab' -Force
- Stop-AzureRmVM -Name 'LinuxB' -ResourceGroupName 'PeeringLab' -Force
- Stop-AzureRmVM -Name 'ImperfectSrv01' -ResourceGroupName 'ImperfectLab' -Force
- Stop-AzureRmVM -Name 'ImperfectLabN02' -ResourceGroupName 'ImperfectLab' -Force
- Stop-AzureRmVM -Name 'ImperfectDC01' -ResourceGroupName 'ImperfectLab' -Force
- Stop-AzureRmVM -Name 'ImperfectDNS' -ResourceGroupName 'ImperfectLab' -Force
+$VSSubID = "8bae2860-70c1-4614-b55a-60e97f4248dc"
+Set-AzureRmContext -SubscriptionID $VSSubID
+
+Get-AzureRmVm | Stop-AzureRmVM -force
+
+ # Stop-AzureRmVM -Name 'UbuntuPlayground' -ResourceGroupName 'OSSWorld' -Force
+ # Stop-AzureRmVM -Name 'JenkMastervm' -ResourceGroupName 'Jenkins' -Force
+ # Stop-AzureRmVM -Name 'JenkSlave0vm' -ResourceGroupName 'Jenkins' -Force
+ # Stop-AzureRmVM -Name 'LinuxA' -ResourceGroupName 'PeeringLab' -Force
+ # Stop-AzureRmVM -Name 'LinuxB' -ResourceGroupName 'PeeringLab' -Force
+# Stop-AzureRmVM -Name 'ImperfectSrv01' -ResourceGroupName 'ImperfectLab' -Force
+#  Stop-AzureRmVM -Name 'ImperfectLabN02' -ResourceGroupName 'ImperfectLab' -Force
+ # Stop-AzureRmVM -Name 'ImperfectDC01' -ResourceGroupName 'ImperfectLab' -Force
+ # Stop-AzureRmVM -Name 'ImperfectDNS' -ResourceGroupName 'ImperfectLab' -Force
